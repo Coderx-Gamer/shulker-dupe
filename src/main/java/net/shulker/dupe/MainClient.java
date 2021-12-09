c5e02e963ef1165eaa73ddeb59d0db5b953940de
Coderx-Gamer <85349822+Coderx-Gamer@users.noreply.github.com>
2021-12-08 19:59:43 -0500

    Add files via upload

--- src/main/java/net/shulker/dupe/MainClient.java
+++ src/main/java/net/shulker/dupe/MainClient.java
@@ -23,12 +23,23 @@ public class MainClient implements ClientModInitializer {
     public void onInitializeClient() {
     }
 
+    public static boolean fra = true;
+    public static int thex = 0;
+    public static int they = 0;
+
+    public static void setFra(boolean fra) {
+        MainClient.fra = fra;
+    }
+
     public static void tick() {
+
+        boolean b1 = (CLIENT.player.currentScreenHandler instanceof ShulkerBoxScreenHandler);
+
         if (shouldDupe || shouldDupeAll) {
             HitResult hit = CLIENT.crosshairTarget;
             if (hit instanceof BlockHitResult) {
                 BlockHitResult blockHit = (BlockHitResult) hit;
-                if (CLIENT.world.getBlockState(blockHit.getBlockPos()).getBlock() instanceof ShulkerBoxBlock && CLIENT.player.currentScreenHandler instanceof ShulkerBoxScreenHandler) {
+                if (CLIENT.world.getBlockState(blockHit.getBlockPos()).getBlock() instanceof ShulkerBoxBlock && b1) {
                     CLIENT.interactionManager.updateBlockBreakingProgress(blockHit.getBlockPos(), Direction.DOWN);
                 } else {
                     log("You need to have a shulker box screen open and look at a shulker box.");
@@ -37,6 +48,11 @@ public class MainClient implements ClientModInitializer {
                     shouldDupeAll = false;
                 }
             }
+        }
+        if (b1) {
+
+        } else {
+            setFra(true);
         }
     }
 }
--- src/main/java/net/shulker/dupe/mixin/ShulkerBoxScreenMixin.java
+++ src/main/java/net/shulker/dupe/mixin/ShulkerBoxScreenMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.shulker.dupe.MainClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,14 +23,31 @@ public class ShulkerBoxScreenMixin extends Screen {
 
     @Inject(at = @At("TAIL"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;IIF)V")
     public void renderScreen(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(232, 60, 50, 20, Text.of("Dupe"), (button) -> {
-            if (shouldDupeAll) shouldDupeAll = false;
-            shouldDupe = true;
-        }));
+        if (isFra()) {
+            setFra(false);
+            MainClient.thex = this.width;
+            MainClient.they = this.height;
+            this.addDrawableChild(new ButtonWidget(this.width/2-90, this.height/2+35-145, 50, 20, Text.of("Dupe"), (button) -> {
+                if (shouldDupeAll) shouldDupeAll = false;
+                shouldDupe = true;
+            }));
+            this.addDrawableChild(new ButtonWidget(this.width/2+40, this.height/2+35-145, 50, 20, Text.of("Dupe All"), (button) -> {
+                if (shouldDupe) shouldDupe = false;
+                shouldDupeAll = true;
+            }));
+        }
 
-        this.addDrawableChild(new ButtonWidget(285, 60, 50, 20, Text.of("Dupe All"), (button) -> {
-            if (shouldDupe) shouldDupe = false;
-            shouldDupeAll = true;
-        }));
+        if (this.width != MainClient.thex || this.height != MainClient.they) {
+            setFra(true);
+        }
+
+    }
+
+    public boolean isFra() {
+        return MainClient.fra;
+    }
+
+    public void setFra(boolean fra) {
+        MainClient.fra = fra;
     }
 }
