b14c287d1a8765e595be7e3bfcd7182b7de5173e
David <85349822+Coderx-Gamer@users.noreply.github.com>
2022-08-22 13:39:17 -0400

    Backdate to 1.19

--- src/main/java/net/shulker/dupe/mixin/ClientConnectionMixin.java
+++ src/main/java/net/shulker/dupe/mixin/ClientConnectionMixin.java
@@ -1,5 +1,7 @@
 package net.shulker.dupe.mixin;
 
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
@@ -8,15 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.shulker.dupe.SharedVariables.shouldDupe;
-import static net.shulker.dupe.SharedVariables.shouldDupeAll;
-import static net.shulker.dupe.Util.quickMoveAllItems;
-import static net.shulker.dupe.Util.quickMoveItem;
+import static net.shulker.dupe.SharedVariables.*;
+import static net.shulker.dupe.Util.*;
 
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
-    @Inject(at = @At("TAIL"), method = "send(Lnet/minecraft/network/Packet;)V")
-    public void send(Packet<?> packet, CallbackInfo ci) {
+    @Inject(at = @At("TAIL"), method = "send(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", cancellable = true)
+    public void send(Packet<?> packet, GenericFutureListener<? extends Future<? super Void>> callback, CallbackInfo ci) {
         if (packet instanceof PlayerActionC2SPacket) {
             if (((PlayerActionC2SPacket) packet).getAction() == PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK) {
                 if (shouldDupe) {
